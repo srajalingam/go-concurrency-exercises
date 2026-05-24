74d3a35290b54ddcf755833209ad8887c5cfb273
srajalingam <rajalingam299@gmail.com>
2026-05-25 00:34:42 +0530

    without waitgroup printed 0, with waitgroup print 1

--- 01-exercise/01-goroutines/03-join/main.go
+++ 01-exercise/01-goroutines/03-join/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
@@ -10,11 +11,14 @@ func main() {
 	// deterministically.
 
 	var data int
+	var wg sync.WaitGroup
+	wg.Add(1)
 
 	go func() {
+		defer wg.Done()
 		data++
 	}()
-
+	wg.Wait()
 	fmt.Printf("the value of data is %v\n", data)
 
 	fmt.Println("Done..")
