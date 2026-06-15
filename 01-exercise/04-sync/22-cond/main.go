948ee13897e5c82fdc853470162453d86e83ba83
srajalingam <rajalingam299@gmail.com>
2026-06-16 00:59:31 +0530

    Broadcast wake up all gorouting

--- 01-exercise/04-sync/22-cond/main.go
+++ 01-exercise/04-sync/22-cond/main.go
@@ -3,25 +3,26 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 var sharedRsc = make(map[string]interface{})
 
 func main() {
 	var wg sync.WaitGroup
+	mu := sync.Mutex{}
+	cond := sync.NewCond(&mu)
 
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 
 		//TODO: suspend goroutine until sharedRsc is populated.
-
+		cond.L.Lock()
 		for len(sharedRsc) == 0 {
-			time.Sleep(1 * time.Millisecond)
+			cond.Wait()
 		}
-
 		fmt.Println(sharedRsc["rsc1"])
+		cond.L.Unlock()
 	}()
 
 	wg.Add(1)
@@ -29,17 +30,19 @@ func main() {
 		defer wg.Done()
 
 		//TODO: suspend goroutine until sharedRsc is populated.
-
+		cond.L.Lock()
 		for len(sharedRsc) == 0 {
-			time.Sleep(1 * time.Millisecond)
+			cond.Wait()
 		}
 
 		fmt.Println(sharedRsc["rsc2"])
+		cond.L.Unlock()
 	}()
-
+	cond.L.Lock()
 	// writes changes to sharedRsc
 	sharedRsc["rsc1"] = "foo"
 	sharedRsc["rsc2"] = "bar"
-
+	cond.Broadcast() //TODO: wake up all goroutines waiting on cond
+	cond.L.Unlock()
 	wg.Wait()
 }
