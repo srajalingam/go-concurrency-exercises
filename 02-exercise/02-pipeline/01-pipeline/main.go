d7b8608e0398902adb027d40b75043281dc8776e
srajalingam <rajalingam299@gmail.com>
2026-06-22 01:12:50 +0530

    Pipeline excercises on using channels

--- 02-exercise/02-pipeline/01-pipeline/main.go
+++ 02-exercise/02-pipeline/01-pipeline/main.go
@@ -1,18 +1,34 @@
 package main
 
+import "fmt"
+
 // TODO: Build a Pipeline
 // generator() -> square() -> print
 
 // generator - convertes a list of integers to a channel
-func generator(nums ...int) {
-
+func generator(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
 }
 
 // square - receive on inbound channel
 // square the number
 // output on outbound channel
-func square() {
-
+func square(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
 }
 
 func main() {
@@ -22,4 +38,10 @@ func main() {
 	// receive the values from square stage
 	// print each one, until channel is closed.
 
+	ch := generator(2, 3)
+	out := square(ch)
+
+	for n := range out {
+		fmt.Println(n)
+	}
 }
