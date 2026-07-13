97443c7a0e75a3ed341ac94cfaf4753970073459
srajalingam <rajalingam299@gmail.com>
2026-07-14 00:10:24 +0530

    Simple excersize for goroutine behaviour

--- 01-exercise/01-goroutines/01-hello/main.go
+++ 01-exercise/01-goroutines/01-hello/main.go
@@ -20,11 +20,23 @@ func main() {
 
 	// goroutine function call
 
+	go fun("1-goroutine")
+
 	// goroutine with anonymous function
+
+	go func() {
+		fun("2-goroutine")
+	}()
 
 	// goroutine with function value call
 
+	fv := fun
+	go fv("3-goroutine")
+
 	// wait for goroutines to end
+
+	fmt.Println("wait for go routines")
+	time.Sleep(100 * time.Microsecond)
 
 	fmt.Println("done..")
 }
